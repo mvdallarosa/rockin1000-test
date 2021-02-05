713b2d1b495b7cbb17dd7a0596329f3a01f6fe4d
Maria Vittoria Dalla Rosa Prati <mvdallarosa@gmail.com>
2021-02-05 17:58:15 +0100

    fixed cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,16 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    if defined?(params) && params[:locale]
-      I18n.locale = I18n.available_locales.include?(params[:locale].to_sym) ? params[:locale] : 'en'
-      set_cookie(I18n.locale)
-    elsif defined?(cookies[:language])
-      I18n.locale = cookies[:language]
+    if defined?(cookies[:language])
+      if defined?(params) && params[:locale]
+        I18n.locale = I18n.available_locales.include?(params[:locale].to_sym) ? params[:locale] : I18n.default_locale
+        set_cookie(I18n.locale)
+      else
+        I18n.locale = cookies[:language]
+      end
     elsif defined?(request.env['HTTP_ACCEPT_LANGUAGE'])
       detected_language = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
-      I18n.locale = I18n.available_locales.include?(detected_language.to_sym) ? detected_language : 'en'
+      I18n.locale = I18n.available_locales.include?(detected_language.to_sym) ? detected_language : I18n.default_locale
       set_cookie(I18n.locale)
     end
     locale = I18n.locale || I18n.default_locale
